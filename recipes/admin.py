e246313d600247b9ccd3d599d3c1c30b2395a476
Virgílio Locatelli Soares <virgiliopaumgartten@gmail.com>
2022-03-08 12:35:25 -0300

    admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Recipe
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(Recipe)
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+admin.site.register(Category, CategoryAdmin)
