077302d3fb36ac7898c162910b8b4333d55abb06
Virgílio Locatelli Soares <virgiliopaumgartten@gmail.com>
2022-03-09 13:02:40 -0300

    404

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
-
+from recipes.models import Recipe
+from django.http import Http404
 from utils.recipe.factory import make_recipe
 
 
 def home(request):
+    recipes = Recipe.objects.filter(is_published = True).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)]
+        'recipes': recipes,
     })
 
+    
+def category(request, category_id):
+    
+    recipes = Recipe.objects.filter(category__id = category_id, is_published=True).order_by('-id')
+    if not recipes:
+        raise Http404('Not Found')
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+        'title': recipes.first().category.name,
+    })    
+    
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
