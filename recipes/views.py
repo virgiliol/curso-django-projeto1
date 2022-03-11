76b9cc7d343831d5c860e99d4b47c0fc00c0ae6d
Virgílio Locatelli Soares <virgiliopaumgartten@gmail.com>
2022-03-11 13:47:42 -0300

    pagination

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,19 @@
+from django.core.paginator import Paginator
 from django.shortcuts import get_list_or_404, get_object_or_404, render
+
 from recipes.models import Recipe
 
 
 def home(request):
     recipes = Recipe.objects.filter(
         is_published=True).order_by('-id')
+
+    current_page = request.GET.get('page', 1)
+    paginator = Paginator(recipes, 10)
+    page_obj = paginator.get_page(current_page)
+
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
     })
 
 
