068add0c2b2cc1817ecf6c513f7159d1fc924f77
Virgílio Locatelli Soares <virgiliopaumgartten@gmail.com>
2022-03-14 12:10:00 -0300

    pagination reutilizar

--- recipes/views.py
+++ recipes/views.py
@@ -1,28 +1,37 @@
-from django.core.paginator import Paginator
+from django.db.models import Q
+from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
+from utils.recipe.pagination import make_pagination
 
 from recipes.models import Recipe
 
 
 def home(request):
     recipes = Recipe.objects.filter(
-        is_published=True).order_by('-id')
+        is_published=True,
+    ).order_by('-id')
 
-    current_page = request.GET.get('page', 1)
-    paginator = Paginator(recipes, 10)
-    page_obj = paginator.get_page(current_page)
+    page_obj, pagination_range = make_pagination(request, recipes, 9)
 
     return render(request, 'recipes/pages/home.html', context={
         'recipes': page_obj,
+        'pagination_range': pagination_range
     })
 
 
 def category(request, category_id):
-    recipes = get_list_or_404(Recipe.objects.filter(
-        category__id=category_id, is_published=True,).order_by('-id')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by('-id')
     )
+
+    page_obj, pagination_range = make_pagination(request, recipes, 9)
+
     return render(request, 'recipes/pages/category.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
         'title': f'{recipes[0].category.name} - Category | '
     })
 
@@ -33,4 +42,29 @@ def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True,
+    })
+
+
+def search(request):
+    search_term = request.GET.get('q', '').strip()
+
+    if not search_term:
+        raise Http404()
+
+    recipes = Recipe.objects.filter(
+        Q(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term),
+        ),
+        is_published=True
+    ).order_by('-id')
+
+    page_obj, pagination_range = make_pagination(request, recipes, 9)
+
+    return render(request, 'recipes/pages/search.html', {
+        'page_title': f'Search for "{search_term}" |',
+        'search_term': search_term,
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
+        'additional_url_query': f'&q={search_term}',
     })
